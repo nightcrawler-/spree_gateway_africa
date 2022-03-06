8d35871ff4f0702809e45b03bfb577297514da17
Frederick Nyawaya <o.frederickn@gmail.com>
2022-03-06 19:55:14 +0300

    fix migrations

--- db/migrate/20220306162530_create_spree_paygo_requests.rb
+++ db/migrate/20220306162530_create_spree_paygo_requests.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateSpreePaygoRequests < ActiveRecord::Migration[7.0]
+class CreateSpreePaygoRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :spree_paygo_requests do |t|
       t.string :account_no
--- db/migrate/20220306162948_add_to_spee_paygo_request.rb
+++ db/migrate/20220306162948_add_to_spee_paygo_request.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-class AddToSpeePaygoRequest < ActiveRecord::Migration[7.0]
+class AddToSpeePaygoRequest < ActiveRecord::Migration[6.1]
   def change
     add_column :spree_paygo_requests, :payment_method_id, :bigint
     add_column :spree_paygo_requests, :user_id, :bigint
-    add_column :spree_paygo_requests, :account_number, :string
-    add_column :spree_paygo_requests, :msisdn, :string
   end
 end
